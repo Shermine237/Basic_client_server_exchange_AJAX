8eed70dd950d6d084ee35f4cb38a784030d9a77c
Charlie Rostant YOSSA <charlieyossa@gmail.com>
2023-03-01 17:42:09 +0100

    Use Fetch() method with async keyword

--- js/post.js
+++ js/post.js
@@ -4,47 +4,49 @@ let txtField = document.getElementById('message')
 
 btn.addEventListener('click', handleClick, false);
 
-function handleClick(e){
+//handleClick
+async function handleClick(e){
     //grab value typed in text field
-    let message = txtField.value;
-    //create xhr
-    makeRequest(message)
-        .then(data => createSuccessHtml(data))
-        .catch(error => createErrorHtml(error))
-        .then( html => updateUI(html))
-        .finally(() => resetForm());
+    let message = document.getElementById('message').value;
+    const reqOptions = {
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({text : message})
+    };
+    try{
+        let rawResponse = await fetch('http://127.0.0.1:3000/server.php', reqOptions);
+        let response = await handleErrors(rawResponse);
+        let data = await response.json();
+        let html = await createSuccessHtml(data);
+    } catch (error){
+        let html =  createErrorHtml(error.message);
+    }
+    finally {
+        updateUI(html);
+    }
+    
 }
 
-function makeRequest(message){
-    return new Promise((resolve, reject) => {
-        let reqBody = {'text' : message}
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://127.0.0.1:3000/server.php');
-        xhr.setRequestHeader('Content-type', 'application/json');
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    //request is a success
-                    resolve(JSON.parse(xhr.responseText))
-                } else {
-                    //request failed
-                    reject(JSON.parse(xhr.responseText))
-                }
-            }
-        }
-        xhr.send(JSON.stringify(reqBody));
-    });
+//handleErrors
+async function handleErrors(response){
+    if(! response.ok){
+        let data = await response.json();
+        throw data;
+    }
+    return response;
 }
 
 function createSuccessHtml(data){
     return html = `<p>${data}</p>`;
 }
 //createErrorHtml
-let createErrorHtml = (data) => {
+let createErrorHtml = (message) => {
 
     return html = `
         <h1>Une erreur s'est produite !</h1>
-        <p>${data.message}</p>
+        <p>${message}</p>
     `
 }
 
@@ -55,7 +57,4 @@ let updateUI = html => {
     response.innerHTML = '';
     //replace with htmlString
     response.insertAdjacentHTML( 'beforeend', html);
-}
-function resetForm(){
-    txtField.value = '';
 }
